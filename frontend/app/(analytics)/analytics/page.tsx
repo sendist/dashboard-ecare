af4a8d2639874da2d4711b3ea6d6da1331cbec2e
sendis <sendisetiawan10902@gmail.com>
2026-03-16 13:29:06 +0700

    feat(frontend): creating template analytics dashboard

---
+++ frontend/app/(analytics)/analytics/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ChartBarMultiple } from "@/components/custom/analytics/bar-chart";
+
+export default function AnalyticsPage() {
+    return (
+        <div className="container px-4 pb-4">
+            <h1 className="text-3xl mb-2">Analytics</h1>
+            <ChartBarMultiple>
+
+            </ChartBarMultiple>
+        </div>
+    )
+}
